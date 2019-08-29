6a653516016b6443831135513910e764aa174005
dawnco <abke@qq.com>
2019-08-29 18:48:47 +0800

    fx

--- main.py
+++ main.py
@@ -2,14 +2,28 @@
 from PIL import Image
 from pytesseract import image_to_string
 
+import time
 import capture
 import util
 
+# https://pypi.org/project/winput/
+from winput import *
+    
 
-
-if __name__ == '__main__':
+def cap():
     file = "data/test.png"
     capture.capture(file)
     img = Image.open(file);
     text = image_to_string(img, lang='chi_sim')
     print(text)
+
+def mouse():
+    set_mouse_pos(90, 556)
+    time.sleep(2) 
+    click_mouse_button(LEFT_MOUSE_BUTTON)
+    # click_mouse_button(RIGHT_MOUSE_BUTTON)
+    # click_key(VK_BACK)
+
+
+if __name__ == '__main__':
+    mouse()
